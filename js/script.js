69c186ec75c72b6a58e1e045a1c5e7dc233207a5
laxmeyers <mey18003@byui.edu>
2022-04-23 10:56:08 -0600

    added the whole page

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Changing the last modified time.
+document.querySelector("#lastmod").textContent = document.lastModified;
+// getting the year that it is.
+document.querySelector("#currentyear").textContent = new Date().getFullYear();

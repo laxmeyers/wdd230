c1198c326bfd725dd1f691d02b4f94cccfb83dc7
laxmeyers <mey18003@byui.edu>
2022-04-26 10:18:16 -0600

    updates

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 // Changing the last modified time.
 document.querySelector("#lastmod").textContent = document.lastModified;
 // getting the year that it is.
-document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.getElementById("currentyear").textContent = new Date().getFullYear();

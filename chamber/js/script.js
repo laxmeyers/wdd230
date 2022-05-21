f7ec1c2302db20d48839f5af8d9a7472024a7a1c
laxmeyers <mey18003@byui.edu>
2022-05-21 10:52:34 -0600

    add content

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -9,7 +9,7 @@ window.onresize = () => {if (window.innerWidth > 760) mainNav.classList.remove('
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-const date = new Date();
+let date = new Date();
 let day = weekday[date.getDay()];
 
 const d = new Date();
@@ -18,3 +18,15 @@ let newName = month[d.getMonth()];
 document.querySelector('#day').textContent = day;
 
 document.querySelector("#date").textContent = `${newName} ${date.getDate()} ${date.getFullYear()}`;
+
+function dayOfWeek(){
+    document.querySelector('#banner').textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+
+console.log(date)
+
+if (date == 1 || date == 2) {
+    dayOfWeek();
+} else {
+    document.querySelector('#banner').style.display='none';
+}

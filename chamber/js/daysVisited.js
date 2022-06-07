cd37333bef9dcfcf6da76508d93afccd387df43c
laxmeyers <mey18003@byui.edu>
2022-06-07 10:03:28 -0600

    home button

--- chamber/js/daysVisited.js
+++ chamber/js/daysVisited.js
@@ -4,13 +4,13 @@ const current = Date.now();
 
 let numVisits;
 
-if (window.localStorage.getItem("visits-ls")) {
-	numVisits = window.localStorage.getItem('visits-ls');
+if (window.localStorage.getItem('visits')) {
+	numVisits = window.localStorage.getItem('visits');
 } else {
 	numVisits = current;
 }
 
-window.localStorage.setItem('visits-ls', current)
+window.localStorage.setItem('visits', current)
 
 const second = Math.floor((current - numVisits) / 1000);
 const lastDay = Math.floor(second / 86400);

f8edc05480511e8da8bbcdec11050d7d7305f7cf
laxmeyers <mey18003@byui.edu>
2022-06-16 12:14:01 -0600

    prophets

---
+++ lesson09/prophets.js
@@ -0,0 +1,30 @@
+const url = "https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json";
+fetch(url)
+.then (response => response.json())
+.then (data => {
+    const prophets = data['prophets']
+    prophets.forEach(displayProphets);
+});
+
+function displayProphets(prophet){
+    let card = document.createElement('section')
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let portrait = document.createElement('img');
+
+    h2.textContent = `${prophet['name']} ${prophet['lastname']}`;
+    p1.textContent = `Birth date: ${prophet['birthdate']}`;
+    p2.textContent = `birth place: ${prophet['birthplace']}`;
+
+    portrait.setAttribute('src', prophet['imageurl']);
+    portrait.setAttribute('alt', `Portrait of ${prophet['name']} ${prophet['lastname']}`);
+    portrait.setAttribute('loading', 'lazy');
+
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(portrait);
+
+    document.querySelector('.cards').appendChild(card)
+}
